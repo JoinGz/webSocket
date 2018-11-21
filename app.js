f4e69b855bcdf4cca65643605a82b96641451870
Gz <354300537@qq.com>
2018-11-21 13:46:10 +0800

    mainly finished

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 // 用来保存对应的socket，就是记录对方的socket实例(私聊时用)
 let socketObj = {}
+// 储存全部进入用户
+let allSocket = {}
 // 监听与客户端的连接事件
 io.on('connection', socket => {
   console.log('服务端连接成功')
+  allSocket[socket.id] = socket
   let username
+  // 记录进入了哪些房间的数组
+  let rooms = []
   // socket 会生成这个对象。
   // 处理客户端传来的函数名，值
   socket.on('message', msg => {
@@ -27,20 +32,52 @@ io.on('connection', socket => {
         if (socketUser) {
           // console.log('用户存在')
           // 要去官网查询下API，只有@的人可以看见，自己都看不见自己说的话了
-          socketUser.emit('allMsg',{
+          socketUser.emit('allMsg', {
+            user: username,
+            msg,
+            createAt: new Date().toLocaleString()
+          })
+          socket.emit('allMsg', {
             user: username,
             msg,
             createAt: new Date().toLocaleString()
           })
         }
       } else {
-        // io.emit()方法是向大厅和所有人房间内的人广播
-        // 接收到用户发送的消息，向全全广播
-        io.emit('allMsg', {
-          user: username,
-          msg,
-          createAt: new Date().toLocaleString()
-        })
+        if (rooms.length) {
+          // 用来存储进入房间内的对应的socket.id
+          let socketJson = {}
+          rooms.forEach(v => {
+            // 取得进入房间内所对应的所有sockets的hash值，它便是拿到的socket.id
+            // console.log(io.sockets);
+            // console.log(io.sockets.adapter.rooms)
+            let roomSockets = io.sockets.adapter.rooms[v].sockets
+            Object.keys(roomSockets).forEach(socketId => {
+              // console.log('socketId', socketId)
+              // 进行一个去重，在socketJson中只有对应唯一的socketId
+              if (!socketJson[socketId]) {
+                socketJson[socketId] = 1
+              }
+            })
+            // console.log(roomSockets);
+          })
+          // console.log(socketJson);
+          Object.keys(socketJson).forEach(id => {
+            allSocket[id].emit('allMsg', {
+              user: username,
+              msg,
+              createAt: new Date().toLocaleString()
+            })
+          })
+        } else {
+          // io.emit()方法是向大厅和所有人房间内的人广播
+          // 接收到用户发送的消息，向全全广播
+          io.emit('allMsg', {
+            user: username,
+            msg,
+            createAt: new Date().toLocaleString()
+          })
+        }
       }
     } else {
       username = msg
@@ -51,6 +88,35 @@ io.on('connection', socket => {
         createAt: new Date().toLocaleString()
       })
       socketObj[username] = socket
+    }
+  })
+  // 加入群聊
+  socket.on('join', data => {
+    // 没进就进来
+    if (username && rooms.indexOf(data) === -1) {
+      socket.join(data)
+      rooms.push(data)
+      socket.emit('joined', {
+        roomName: data
+      })
+      socket.emit('allMsg', {
+        user: '系统',
+        msg: `进入了${data}战队 <br /> 以下内容为战队内聊天`,
+        createAt: new Date().toLocaleString()
+      })
+    }
+  })
+  socket.on('leave', data => {
+    let index = rooms.indexOf(data)
+    if (index !== -1) {
+      socket.leave(data)
+      rooms.splice(index, 1)
+      // socket.emit('leaved',data)
+      socket.emit('allMsg', {
+        user: '系统',
+        msg: `离开了${data}战队 <br /> 以下内容为大厅聊天`,
+        createAt: new Date().toLocaleString()
+      })
     }
   })
 })
